705df42b6923763303abbd2deb3ba98683a48193
Oliver <oliverjamarie@gmail.com>
2020-11-18 13:52:04 +0100

    Main update

--- Code/Main.cpp
+++ Code/Main.cpp
@@ -2,11 +2,14 @@
 #include "Notebook.h"
 #include "Topic.h"
 #include "LinkedList.h"
-#include <exception>
+#include <exception> // not required for full release
 #include "BinarySearchTree.h"
-#include <ctime>
+#include <ctime> // not required for full release, used to generate random numbers in testing
+#include <vector>
 
 using namespace std;
+
+void testVector(vector<int> &in);
 
 void testLinkedList(){
 LinkedList<int>* list = new LinkedList<int>();
@@ -63,30 +66,58 @@ LinkedList<int>* list = new LinkedList<int>();
 
 }
 
-
 void testBST(){
     BinarySearchTree<int>* bst = new BinarySearchTree<int>();
     srand((unsigned) time(0));
     int num ;
+
+    // Root
+
     for (int i = 0; i < 10; i ++){
         num = rand() % 100;
         cout << "ADDING\t" << num << endl;
         bst->insert(num);
     }
-    
-    bst->display(0);
-    bst->display(1);
-    bst->display(2);
-}
 
+    cout << "\n\nSIZE\t" << bst->getSize() <<endl;
+    bst->display(2);
+
+    bst->balance();
+    bst->display(2);
+
+    //delete bst;
+}
+void testVector(vector<int> &in){
+    int size = in.size();
+
+    for (int i = 0; i < size; i ++){
+        cout << in[i] <<endl;
+        //in[i] ++;
+    }
+
+}
 int main (){
     Notebook note =  Notebook("Test Notebook");
     Notebook other =  Notebook("Test Notebook");
     Topic *topic = new Topic(0,"French");
+    vector<int> set;
     
-    //testBST();
+    /* for (int i = 0; i < 20; i ++){
+        set.push_back(i);
+    }
 
-    testLinkedList();
+    testVector(set);
+
+    for (int i : set){
+        cout << i <<endl;
+    } */
+
+    
+
+    testBST();
+
+
+    //testLinkedList();
 
     return 0;
 }
