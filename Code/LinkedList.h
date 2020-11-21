c80754250aacc9c040e9ff6152f381c293286a49
Oliver <oliverjamarie@gmail.com>
2020-11-21 17:51:20 +0100

    Linked List Update
    Adds support for sorting list
    Adds copy constructor for node and list
    Adds support for inserting a vector

--- Code/LinkedList.h
+++ Code/LinkedList.h
@@ -6,7 +6,7 @@
 
 #include <iostream>
 #include <exception>
-
+#include <vector>
 #include "Compare.h"
 using namespace std;
 
@@ -20,6 +20,10 @@ class LinkedListNode{
     public:
         LinkedListNode(T data_in){
             data = data_in;
+        }
+        LinkedListNode(LinkedListNode<T> &other){
+           data = other.data;
+           next = other.next;
         }
 
         //Functions
@@ -46,32 +50,100 @@ class LinkedList{
         LinkedList();
         LinkedList(T);
         LinkedList(LinkedListNode<T>*);
+        LinkedList(LinkedList<T> &other){
+            head = new LinkedListNode<T>(other.head);
+            size = other.size;
+        }
 
 
         //Functions
         bool append(T);
+        bool append(vector<T>);
         bool remove(T);
-
+        void sort();
         LinkedListNode<T>* find(T);
         LinkedListNode<T>* getHead(){
             return head;
         }
-
         int index(T);
         int getSize(){
             return size;
         }
+        vector<T> convertToVector();
 
     private:
-
+        //Variables
         LinkedListNode<T>* head;
         int size;
+
+        //Functions
+        void sortVector(vector<T> vector);
+        void clear ();
+
 };
 
 TEMP
 LinkedList<T>::LinkedList(){
     head = NULL;
     size = 0;
+}
+
+TEMP 
+void LinkedList<T>::sort(){
+    vector<T> set = convertToVector();
+    
+    clear();
+
+
+    //bubble sort
+    for (int i = 0; i < set.size() - 1; i ++){
+        for (int j = 0; j < set.size() - 1 - i; j++){
+            if (Compare<T>::compare(set[j], set[j+1]) > 0){
+                T temp;
+
+                temp = set[j];
+                set[j] = set[j+1];
+                set[j+1] = temp;
+            }
+        }
+    }
+
+    append(set);
+}
+
+TEMP
+vector<T> LinkedList<T>::convertToVector(){
+    vector<T> set;
+    LinkedListNode<T>* curr = head;
+
+    while (curr != NULL){
+        set.push_back(curr->data);
+        curr = curr->next;
+    }
+
+    return set;
+}
+
+TEMP 
+void LinkedList<T>::clear(){
+    LinkedListNode<T>* prev;
+    LinkedListNode<T>* curr ;
+
+    if (head == NULL){
+        return;
+    }
+
+    curr = head;
+
+    while (curr != NULL){
+        cout << curr->data << endl;
+        prev = curr;
+        curr = curr->next;
+        delete prev;
+    }
+
+    size = 0;
+    head = NULL;
 }
 
 TEMP
@@ -109,8 +181,19 @@ bool LinkedList<T>::append(T data_in){
     return true;
 }
 
+TEMP
+bool LinkedList<T>::append(vector<T> set){
+    for (T i : set){
+        if (append(i) == false){
+            return false;
+        }
+    }
+
+    return true;
+}
+
 // Searches for node holding specified data in the list and returns it
-// Reurns null if not found
+// Returns null if not found
 TEMP 
 LinkedListNode<T>* LinkedList<T>::find(T data){
     LinkedListNode<T>* curr = head;
